1d219a7aa537ed1660117425573c7777a45897c8
Matthew Spero <watercycle@email.com>
2023-11-10 12:06:33 -0600

    Batch pt-online-schema-change statements

---
+++ src/BatchableBlueprint.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Daursu\ZeroDowntimeMigration;
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Connection;
+use Illuminate\Database\Schema\Grammars\Grammar;
+
+/**
+ * A variant of `Blueprint` that allows for connection types to define a `statements`
+ * function to process an array of SQL query strings at once.
+ */
+class BatchableBlueprint extends Blueprint
+{
+    /**
+     * @see Blueprint::build
+     */
+    public function build(Connection $connection, Grammar $grammar)
+    {
+        $statements = $this->toSql($connection, $grammar);
+
+        // Allow connections to run multiple statements at once if they support it.
+        // For example, pt-online-schema-change does for running multiple operations.
+        // on the cloned / "_new" table.
+        if (!empty($statements) && method_exists($connection, 'statements')) {
+            return $connection->statements($statements);
+        }
+
+        // Default / "parent" logic - but re-using `$statements`.
+        foreach ($statements as $statement) {
+            $connection->statement($statement);
+        }
+    }
+}
